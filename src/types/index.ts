fd1ae389a9d35b6d6d01dbfa91c9f375bbc66605
samanes11 <samanes0011@gmail.com>
2026-09-02 17:35:35 +0330

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -96,8 +96,13 @@ export interface CurrencyQuote {
 
 export interface WeatherData {
   temperatureC: number;
+  feelsLikeC: number;      
   condition: string;
   humidity: number;
   windKph: number;
   locationName: string;
+  tempMaxC: number;        
+  tempMinC: number;        
+  sunrise: string;         
+  sunset: string;         
 }
